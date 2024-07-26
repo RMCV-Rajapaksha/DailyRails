23b479a2b65807f6eb3718ee23bd51110182dfa4
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-07-27 00:50:15 +0530

    refactor: Update SubmitItem page UI and form fields

--- website/src/pages/SubmitItem.js
+++ website/src/pages/SubmitItem.js
@@ -1,39 +1,45 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 function SubmitItem() {
   return (
     <>
-    <Navbar/>
-    
-<div className='mt-20 mb-20'>
-<form class="max-w-sm mx-auto">
-  <div class="mb-5">
-    <label for="email" class="block mb-2 text-sm  text-primary font-body">Your email</label>
-    <input type="email" id="email" class="shadow-sm bg-white border border-gray-300 text-secondary-1 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
-  </div>
-  <div class="flex items-start mb-5">
-    <div class="flex items-center h-5">
-      <input id="terms" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-    </div>
-    <label for="terms" class="ms-2 text-sm font-medium text-primary">Lost</label>
-    
-  </div>
-  <div class="mb-5">
-    <label for="password" class="block mb-2 text-sm  text-primary font-body">Your password</label>
-    <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-  </div>
-  <div class="mb-5">
-    <label for="repeat-password" class="block mb-2 text-sm  text-primary font-body">Repeat password</label>
-    <input type="password" id="repeat-password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-  </div>
-  
-  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
-</form>
-</div>
-    <Footer/>
+      <Navbar />
+      <div className='mt-20 mb-20 font-body'>
+        <form className="max-w-sm mx-auto">
+          <div className="mb-5">
+            <label htmlFor="name" className="block mb-2 text-sm text-primary font-body">Name</label>
+            <input type="text" id="name" className="shadow-sm bg-gray-50 border border-secondary-1 text-tertiary text-sm rounded-sm focus:border-primary block w-full p-2.5" placeholder="name@flowbite.com" required />
+          </div>
+          <div className="flex items-start mb-5">
+            <div className="flex items-center h-5">
+              <input id="lost" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+              <label htmlFor="lost" className="ms-2 text-sm font-medium text-primary">Lost</label>
+            </div>
+            <div className="flex items-center h-5 ml-4">
+              <input id="found" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+              <label htmlFor="found" className="ms-2 text-sm font-medium text-primary">Found</label>
+            </div>
+          </div>
+          <div className="mb-5">
+            <label htmlFor="title" className="block mb-2 text-sm text-primary font-body">Title</label>
+            <input type="text" id="title" className="shadow-sm bg-gray-50 border border-secondary-1 text-tertiary text-sm rounded-sm focus:border-primary block w-full p-2.5" required />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="description" className="block mb-2 text-sm text-primary font-body">Description</label>
+            <textarea id="description" className="shadow-sm bg-gray-50 border border-secondary-1 text-tertiary text-sm rounded-sm focus:border-primary block w-full p-2.5 h-24" required></textarea>
+          </div>
+          <div className="mb-5">
+            <label htmlFor="contact" className="block mb-2 text-sm text-primary font-body">Contact Number</label>
+            <input type="text" id="contact" className="shadow-sm bg-gray-50 border border-secondary-1 text-tertiary text-sm rounded-sm focus:border-primary block w-full p-2.5" required />
+          </div>
+          <button type="submit" className="text-white bg-primary hover:bg-secondary p-3 rounded-sm">Submit</button>
+        </form>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default SubmitItem
+export default SubmitItem;
