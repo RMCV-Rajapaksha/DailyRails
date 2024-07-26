c6c751d5cea200e9621d8e1f8d3228fddf5042ec
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-07-26 18:58:07 +0530

    Add responsive navigation bar with menu toggle

--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,13 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <div className="bg-secondary">
-      <h1 className="text-white text-xl font-body">My first React App</h1>
-    </div>
+    <h1 className="text-white text-xl font-body">My first React App</h1>
+  </div>
+  </>
   );
 }
 
