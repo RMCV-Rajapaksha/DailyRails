16c58a5ffa65fbb748e3c39da5145ca990cb07aa
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-10-01 14:54:59 +0530

    Refactor UserValidators.js to add validation for new user registration and login credentials

---
+++ backend/Features/Auth/validators/UserValidators.js
@@ -0,0 +1,48 @@
+const { body, param } = require("express-validator");
+
+const validateNewUser = [
+  body("Name")
+    .isString()
+    .withMessage("Name must be a string")
+    .isLength({ min: 1, max: 50 })
+    .withMessage("Name should be between 1 and 50 characters"),
+
+  body("Email")
+    .isEmail()
+    .withMessage("Email must be a valid email address")
+    .isLength({ max: 100 })
+    .withMessage("Email should not exceed 100 characters"),
+
+  body("Password")
+    .isStrongPassword()
+    .withMessage(
+      "Password must be strong (min 8 chars, include uppercase, lowercase, number, and symbol)"
+    ),
+];
+
+const validateLogin = [
+  body("Email")
+    .isEmail()
+    .withMessage("Email must be a valid email address")
+    .isLength({ max: 100 })
+    .withMessage("Email should not exceed 100 characters"),
+
+  body("Password")
+    .isString()
+    .withMessage("Password must be a string")
+    .isLength({ min: 8 })
+    .withMessage("Password should be at least 8 characters long")
+    .withMessage(
+      "Password must include uppercase, lowercase, numbers, and symbols"
+    ),
+];
+
+const validateUserId = [
+  param("ID").isInt().withMessage("User ID must be an integer"),
+];
+
+module.exports = {
+  validateNewUser,
+  validateUserId,
+  validateLogin,
+};
