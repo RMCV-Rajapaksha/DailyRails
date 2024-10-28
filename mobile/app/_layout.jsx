113f4a8a4880e326574a267ae77507dc8f7bc71d
VirangaWeerabandara <umiduviranga8@gmail.com>
2024-10-28 16:51:09 +0530

    Refactor tab layout and add navigation options for features/(tabs)

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -26,6 +26,7 @@ const RootLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{headerShown: false}} />
+      <Stack.Screen name="features/(tabs)" options={{ headerShown: true }} />
     </Stack>
   )
 }
