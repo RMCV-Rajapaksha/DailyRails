99cc9021c7efa4b56ea2a593cd858eec55a0fa01
RMCV-Rajapaksha <cam.chamara@gmail.com>
2024-10-31 06:36:38 +0530

    Refactor Button component to use framer-motion for animations

--- website/src/components/Button.js
+++ website/src/components/Button.js
@@ -1,15 +1,28 @@
-// src/components/Button.js
 import React from "react";
+import { motion } from "framer-motion";
 
 const Button = ({ type = "button", onClick, children, className }) => {
   return (
-    <button
+    <motion.button
       type={type}
       onClick={onClick}
-      className={`p-3 text-white rounded-sm bg-primary hover:bg-secondary ${className}`}
+      className={`p-3 text-white rounded-sm bg-primary ${className}`}
+      whileHover={{
+        scale: 1.05,
+        backgroundColor: "var(--secondary)",
+      }}
+      whileTap={{ scale: 0.95 }}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.2,
+        ease: "easeInOut",
+        opacity: { duration: 0.2 },
+        y: { duration: 0.2 },
+      }}
     >
       {children}
-    </button>
+    </motion.button>
   );
 };
 
