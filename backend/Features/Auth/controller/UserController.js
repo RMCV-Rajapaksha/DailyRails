efee57f842439fdc0f5cff69eb03ab0aa0375637
RMCV-Rajapaksha <cam.chamara@gmail.com>
2025-01-05 12:26:32 +0530

    Add admin update functionality and user login logic with JWT token generation

--- backend/Features/Auth/controller/UserController.js
+++ backend/Features/Auth/controller/UserController.js
@@ -24,7 +24,51 @@ const postUser = async (req, res) => {
     res.status(500).json({ error: "Failed to create User" });
   }
 };
+const userLogin = async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { Email: req.body.Email } });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const validPassword = await bcrypt.compare(
+      req.body.Password,
+      user.Password
+    );
+
+    if (!validPassword) {
+      return res.status(400).json({ error: "Invalid password" });
+    }
+
+    const jwtToken = jwt.sign(
+      {
+        ID: user.ID,
+        Email: user.Email,
+        Name: user.Name,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "3d",
+      }
+    );
+
+    res.json({ message: "Login successful", token: jwtToken });
+  } catch (error) {
+    console.error("Error details:", error);
+    res.status(500).json({ error: "Failed to login" });
+  }
+};
+
+const adminLogout = async (req, res) => {
+  try {
+    res.clearCookie("token", { sameSite: "None", secure: true });
+    res.status(200).json("User Logged out");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
 module.exports = {
   postUser,
+  userLogin,
+  adminLogout,
 };
